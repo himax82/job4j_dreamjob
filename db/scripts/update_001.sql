c289440fcb079342292d25f91d291da0c4a7d3e9
himax82 <himax82@gmail.com>
2021-10-27 08:24:21 +0800

    3. Тесты на DbStore [#504817]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,25 +1,26 @@
 CREATE TABLE if not exists post (
                       id SERIAL PRIMARY KEY,
-                      name TEXT,
+                      name varchar(200),
                       created timestamp
+);
+
+create table if not exists city
+(
+    id            serial primary key,
+    name          varchar(200)
 );
 
 CREATE TABLE if not exists candidate (
                            id SERIAL PRIMARY KEY,
-                           name TEXT,
+                           name varchar(200),
                            cityId int references city (id),
                            created timestamp
 );
 
 CREATE TABLE if not exists users (
                        id SERIAL PRIMARY KEY,
-                       name TEXT UNIQUE,
-                       email TEXT,
-                       password TEXT
+                       name varchar(200),
+                       email varchar(200) UNIQUE,
+                       password varchar(200)
 );
 
-create table if not exists city
-(
-    id            serial primary key,
-    name          text
-);
